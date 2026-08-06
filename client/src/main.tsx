2535ca01b921ffcf8f2d6d056dcbe62027df7bd9
Priyanshu-Chouhan <priyanshuchouhan185@gmail.com>
2026-08-07 01:30:11 +0530

    feat: switch from BrowserRouter to HashRouter for routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import './index.css';
@@ -9,9 +9,9 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 );
